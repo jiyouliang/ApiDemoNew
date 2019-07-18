5e73e7569613a41045a82aa804101e6529fa7ea8
jiyouliang <jeyouliang@gmail.com>
2019-07-18 11:56:58 +0800

    动态波浪线完成

--- samplesApiDemos/src/main/java/com/example/android/apis/mydemo/vectordrawable/BezierWaveActivity.java
+++ samplesApiDemos/src/main/java/com/example/android/apis/mydemo/vectordrawable/BezierWaveActivity.java
@@ -3,6 +3,7 @@ package com.example.android.apis.mydemo.vectordrawable;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.android.apis.R;
 
@@ -12,5 +13,6 @@ public class BezierWaveActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bezier_wave);
+        Toast.makeText(this, "点击显示波浪动画", Toast.LENGTH_SHORT).show();
     }
 }
--- samplesApiDemos/src/main/java/com/example/android/apis/mydemo/vectordrawable/BezierWaveView.java
+++ samplesApiDemos/src/main/java/com/example/android/apis/mydemo/vectordrawable/BezierWaveView.java
@@ -1,119 +1,99 @@
 package com.example.android.apis.mydemo.vectordrawable;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
-/**
- * @author YouLiang.Ji
- * 贝塞尔曲线实现波浪效果
- */
-public class BezierWaveView extends View {
-    private Paint mPaint;
+public class BezierWaveView extends View implements View.OnClickListener {
+
+
     private Path mPath;
-    //起点
-    private float mStartX;
-    private float mStartY;
-    //终点
-    private float mEndX;
-    private float mEndY;
-    //控制点
-    private float mFlagX;
-    private float mFlagY;
-    //每个控制点高度
-    private int mFlagHeight;
-    //波浪长度
-    private int mWaveLength;
-    //波浪个数
-    private int mWaveCount;
 
+    private Paint mPaintBezier;
+
+    private int mWaveLength;
+    private int mScreenHeight;
+    private int mScreenWidth;
+    private int mCenterY;
+    private int mWaveCount;
+    private static final String TAG = "BezierWaveView";
+
+    private ValueAnimator mValueAnimator;
+    private int mOffset;
 
     public BezierWaveView(Context context) {
-        this(context, null);
+        super(context);
     }
 
-    public BezierWaveView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
+    public BezierWaveView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaintBezier = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBezier.setColor(Color.RED);
+        mPaintBezier.setStrokeWidth(8);
+        mPaintBezier.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        mWaveLength = 160;
     }
 
-    public BezierWaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public BezierWaveView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
-        mPath = new Path();
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setStrokeWidth(5);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(Color.RED);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        //起点y轴在屏幕中线
-        mStartX = 0;
-        mStartY = h / 2;
-        mEndY = mStartY;
+        mPath = new Path();
+        setOnClickListener(this);
 
-        mWaveLength = 100;
-        //四舍五入
-        mWaveCount = Math.round((float) (w / mWaveLength) + 1.5f);
-        mFlagHeight = mWaveLength / 2;
+        mScreenHeight = h;
+        mScreenWidth = w;
+        mCenterY = h / 2;
 
+        mWaveCount = (int) Math.round(w / mWaveLength + 1.5);
+        log("onSizeChanged: mWaveCount=" + mWaveCount);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        //绘制波浪线
         mPath.reset();
-        mPath.moveTo(mStartX, mStartY);
-        //绘制完整波浪线
+        mPath.moveTo(-mWaveLength + mOffset, mCenterY);
         for (int i = 0; i < mWaveCount; i++) {
-            mFlagX = mStartX + mWaveLength / 2 + mWaveLength * 2 * i;
-            mFlagY = mStartY + mFlagHeight;
-            mEndX = mWaveLength + mWaveLength * 2 * i;
-            mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
-
-            mFlagX = mStartX + mWaveLength / 2 + mWaveLength + mWaveLength * 2 * i;
-            mFlagY = mStartY - mFlagHeight;
-            mEndX = mWaveLength + mWaveLength + mWaveLength * 2 * i;
-            mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
+            log("onDraw foreach:  i=" + i + ", mOffset = " + mOffset);
+            mPath.quadTo(-mWaveLength * 3 / 4 + i * mWaveLength + mOffset, mCenterY + 60, -mWaveLength / 2 + i * mWaveLength + mOffset, mCenterY);
+            mPath.quadTo(-mWaveLength / 4 + i * mWaveLength + mOffset, mCenterY - 60, i * mWaveLength + mOffset, mCenterY);
         }
+        mPath.lineTo(mScreenWidth, mScreenHeight);
+        mPath.lineTo(0, mScreenHeight);
+        mPath.close();
+        canvas.drawPath(mPath, mPaintBezier);
+    }
 
+    @Override
+    public void onClick(View view) {
+        mValueAnimator = ValueAnimator.ofInt(0, mWaveLength);
+        mValueAnimator.setDuration(1000);
+        mValueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mValueAnimator.setInterpolator(new LinearInterpolator());
+        mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mOffset = (int) valueAnimator.getAnimatedValue();
+                invalidate();
+                log("onAnimationUpdate:  mOffset = " + mOffset);
+            }
+        });
+        mValueAnimator.start();
+    }
 
-
-
-//        // i = 1
-//        mFlagX = mStartX + mWaveLength / 2 + mWaveLength * 2;
-//        mFlagY = mStartY + mFlagHeight;
-//        mEndX = mWaveLength + mWaveLength * 2;
-//        mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
-//
-//        mFlagX = mStartX + mWaveLength / 2 + mWaveLength + mWaveLength * 2;
-//        mFlagY = mStartY - mFlagHeight;
-//        mEndX = mWaveLength + mWaveLength + mWaveLength * 2;
-//        mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
-//
-//        // i = 2
-//        mFlagX = mStartX + mWaveLength / 2 + mWaveLength * 4;
-//        mFlagY = mStartY + mFlagHeight;
-//        mEndX = mWaveLength + mWaveLength * 2 + mWaveLength * 2;
-//        mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
-//
-//
-//        mFlagX = mStartX + mWaveLength / 2 + mWaveLength + mWaveLength * 2 + mWaveLength * 2;
-//        mFlagY = mStartY - mFlagHeight;
-//        mEndX = mWaveLength + mWaveLength + mWaveLength * 2 + mWaveLength * 2;
-//        mPath.quadTo(mFlagX, mFlagY, mEndX, mEndY);
-
-
-        canvas.drawPath(mPath, mPaint);
-
+    private void log(String msg) {
+        Log.d(TAG, msg);
     }
 }
